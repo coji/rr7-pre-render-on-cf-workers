4d3d680a1b270e0f18e7d6c01c2a80f49432bdd4
coji <coji@techtalk.jp>
2024-11-21 10:49:19 +0900

    feat: add Cloudflare Workers support with pre-rendering and updated configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { reactRouter } from '@react-router/dev/vite';
+import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare';
+import { getLoadContext } from './load-context';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    cloudflareDevProxy({ getLoadContext }),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+  ssr: { resolve: { conditions: ['workerd', 'worker', 'browser'] } },
+  resolve: { mainFields: ['browser', 'module', 'main'] },
 });
